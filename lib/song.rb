cba0f5511c36c2fd00fe77c5b06f102218cdaf3c
Remmy Clay <remmy.lauren@gmail.com>
2017-11-23 02:43:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,9 @@ attr_accessor :name, :artist, :genre
     genre_count = {}
     @@genres.each do |genre|
       if genre_count.has_key?(genre)
-        genre_count[genre]=1
-      else
         genre_count[genre]+=1
+      else
+        genre_count[genre]=  1
 end
 
   #    genre_count[genre] ||= genre_count[genre] => 0
